a478e9793961297171b4edbb47f6ba4b6b7d1818
Cecilia Aponte <ccapontep@gmail.com>
2019-08-06 15:49:18 +0200

    Add existing file

--- scripts/ER_modim.py
+++ scripts/ER_modim.py
@@ -33,11 +33,11 @@ def i1():
     # im.executeModality('TEXT_default',a,', I am a new patient.')
 
 
-    if a == 'yes' && a != 'timeout':
+    if a == 'yes' and a != 'timeout':
         im.executeModality('TEXT_default','Yes, I am a new patient.')
         time.sleep(3)
         im.display.loadUrl('ERindex.html')
-    elif a == 'no' && a != 'timeout':
+    elif a == 'no' and a != 'timeout':
         im.executeModality('TEXT_default','No, I am a patient in the database.')
         time.sleep(3)
     elif a == 'timeout':
