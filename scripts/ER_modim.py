1105bc0ee1b78832c7902a0ccb531b5a26627cf6
Cecilia Aponte <ccapontep@gmail.com>
2019-08-06 19:29:15 +0200

    Add existing file

--- scripts/ER_modim.py
+++ scripts/ER_modim.py
@@ -20,7 +20,7 @@ mc.setCmdServerAddr(cmdsever_ip, cmdserver_port)
 def i1():
     # im.setDemoPath("/home/ubuntu/playground/HumanRobotInteraction_ER")
     # im.gitpull()
-    im.__init__()
+    im.robot()
     im.display.loadUrl('HRIER/ERslide.html')
 
     im.executeModality('TEXT_title','Welcome to Wellness Hospital!')
