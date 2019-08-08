de27cde1c5adf66c83344505380d72cda7ee1ed4
Cecilia Aponte <ccapontep@gmail.com>
2019-08-08 10:21:14 +0200

    Add existing file

--- scripts/ER_modim.py
+++ scripts/ER_modim.py
@@ -98,9 +98,9 @@ def i2():
 
     ticketNumber = str(int(Num1)*100 + int(Num2)*10 + int(Num3))
     say('Your ticket number is '+ticketNumber)
-    if ticketNumber < 10:
+    if int(ticketNumber) < 10:
         ticketNumber = "00" + ticketNumber
-    elif ticketNumber < 100:
+    elif int(ticketNumber) < 100:
         ticketNumber = "0" + ticketNumber
     im.executeModality('TEXT_default', 'yes')
     time.sleep(3)
