5af635c1711f84463d07efff933f059be4b12c4a
Cecilia Aponte <ccapontep@gmail.com>
2019-08-08 10:47:23 +0200

    Add existing file

--- scripts/ER_modim.py
+++ scripts/ER_modim.py
@@ -76,7 +76,7 @@ def i2():
 
     # There are three digits in the ticket number, check one by one with buttons
     # First number:
-    CorrTick == 'no'
+    CorrTick = 'no'
     while CorrTick == 'no':
         im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
         im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
