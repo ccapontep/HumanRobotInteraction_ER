b6cbc89c7a4b8fdc014783149dd1023b46baffce
Cecilia Aponte <ccapontep@gmail.com>
2019-08-08 18:33:14 +0200

    Add existing file

--- scripts/ER_modim.py
+++ scripts/ER_modim.py
@@ -153,7 +153,7 @@ def i2():
     with open(os.path.join(directory, RecordTxt), "r") as record:
         recordStr = str(record.read())
         recordStr = re.sub('\n', '', recordStr)
-        # im.executeModality('TEXT_default', str(recordStr.keys()))
+        im.executeModality('TEXT_default', str(type(record)))
         # recordStr = re.sub('\n', '', recordStr)
         RecordDict = {recordStr}
         say('yes 1', 'en')
