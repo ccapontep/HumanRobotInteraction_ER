094ae8ccf8bb071959ae1ca88e484bac0f59dc42
Cecilia Aponte <ccapontep@gmail.com>
2019-08-09 14:12:06 +0200

    Add existing file

--- scripts/ER_modim.py
+++ scripts/ER_modim.py
@@ -231,7 +231,7 @@ def i2():
             if UserQues == 'history':
                 im.executeModality('TEXT_default', 'Select again all that apply.')
                 hist2add = ''
-                while:
+                while True:
                     im.executeModality('BUTTONS',[['weight','OverweightorObese'],['cigar','SmokeCigarettes'], ['chol', 'HighCholesterol'], ['hyper', 'Hypertension'], ['diabetes', 'Diabetes'], ['againSym', 'CurrentSymptomsRecurring'], ['done', 'Done, exit.']])
                     im.executeModality('ASR',['weight','cigar', 'chol', 'hyper', 'diabetes', 'againSym', 'done'])
                     histQ = im.ask(actionname=None, timeoutvalue=10000)
