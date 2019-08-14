4a53bf05ec99f7cb3ca995e63908ed011bc7c2c2
Cecilia Aponte <ccapontep@gmail.com>
2019-08-14 17:10:45 +0200

    Add existing file

--- scripts/ER_modim.py
+++ scripts/ER_modim.py
@@ -88,67 +88,6 @@ def i1():
 
     end()
 
-# Function to get the ticket number
-def getTicket():
-    im.executeModality('TEXT_default', 'worked or not????')
-    # First number:
-    im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
-    im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
-    Num1 = im.ask(actionname=None, timeoutvalue=1000)
-    say('number '+Num1)
-    im.display.remove_buttons()
-
-    # Second number:
-    im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
-    im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
-    Num2 = im.ask(actionname=None, timeoutvalue=200)
-    say('number'+Num2)
-    im.display.remove_buttons()
-
-    # Second number:
-    im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
-    im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
-    Num3 = im.ask(actionname=None, timeoutvalue=200)
-    say('number'+Num3)
-    im.display.remove_buttons()
-
-    # Final ticket number
-    ticketNumber = str(Num1 + Num2 + Num3)
-    time.sleep(1)
-    ticketStr = 'Your ticket number is: ' + ticketNumber
-    im.executeModality('TEXT_default', ticketStr)
-    say('Your ticket number is '+ticketNumber)
-
-    # Check if ticket was entered correctly
-    im.executeModality('BUTTONS',[['yes','Yes'],['no','No']])
-    im.executeModality('ASR',['yes','no'])
-    CorrTick = im.ask(actionname=None, timeoutvalue=100)
-    im.display.remove_buttons()
-    if CorrTick == 'yes':
-        say('Great! Let me look at your information')
-    elif CorrTick == 'no':
-        im.executeModality('TEXT_default', 'Please enter again the digits of your ticket number one by one.')
-        say('Sorry about that. Let us try again')
-
-    # Check the tickets in the system
-    directory = "/home/ubuntu/playground/HumanRobotInteraction_ER/patientInfo"
-
-    ticketNums = []
-    with open(os.path.join(directory, "PatientTicketNum.txt"), "r") as patientTicketNums:
-        for ticket in patientTicketNums.readlines():
-            ticketNums.append(str(ticket))
-    if CorrTick == 'yes' and len(ticketNums) > 0 and int(ticketNumber) in (map(int, ticketNums)):
-        im.executeModality('TEXT_default', 'Your ticket has been found!')
-        say('Your ticket has been found in the database', 'en')
-        CorrTick == 'yes'
-        # break
-    elif CorrTick == 'yes' and int(ticketNumber) not in (map(int, ticketNums)):
-        im.executeModality('TEXT_default', 'Sorry. Your ticket was not found.')
-        say('Your ticket was not found. Let us start again.', 'en')
-        im.executeModality('TEXT_default', 'Please enter again the digits of your ticket number one by one.')
-        CorrTick = 'no'
-
-    return(CorrTick, ticketNumber)
 
 # Interaction to check ticket info and retrieve info for the user
 def i2():
@@ -163,63 +102,62 @@ def i2():
     # There are three digits in the ticket number, check one by one with buttons
     CorrTick = 'no'
     while CorrTick == 'no':
-        CorrTick, ticketNumber = getTicket()
-        # # First number:
-        # im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
-        # im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
-        # Num1 = im.ask(actionname=None, timeoutvalue=1000)
-        # say('number '+Num1)
-        # im.display.remove_buttons()
-        #
-        # # Second number:
-        # im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
-        # im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
-        # Num2 = im.ask(actionname=None, timeoutvalue=200)
-        # say('number'+Num2)
-        # im.display.remove_buttons()
-        #
-        # # Second number:
-        # im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
-        # im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
-        # Num3 = im.ask(actionname=None, timeoutvalue=200)
-        # say('number'+Num3)
-        # im.display.remove_buttons()
-        #
-        # # Final ticket number
-        # ticketNumber = str(Num1 + Num2 + Num3)
-        # time.sleep(1)
-        # ticketStr = 'Your ticket number is: ' + ticketNumber
-        # im.executeModality('TEXT_default', ticketStr)
-        # say('Your ticket number is '+ticketNumber)
-        #
-        # # Check if ticket was entered correctly
-        # im.executeModality('BUTTONS',[['yes','Yes'],['no','No']])
-        # im.executeModality('ASR',['yes','no'])
-        # CorrTick = im.ask(actionname=None, timeoutvalue=100)
-        # im.display.remove_buttons()
-        # if CorrTick == 'yes':
-        #     say('Great! Let me look at your information')
-        # elif CorrTick == 'no':
-        #     im.executeModality('TEXT_default', 'Please enter again the digits of your ticket number one by one.')
-        #     say('Sorry about that. Let us try again')
-        #
-        # # Check the tickets in the system
-        # directory = "/home/ubuntu/playground/HumanRobotInteraction_ER/patientInfo"
-        #
-        # ticketNums = []
-        # with open(os.path.join(directory, "PatientTicketNum.txt"), "r") as patientTicketNums:
-        #     for ticket in patientTicketNums.readlines():
-        #         ticketNums.append(str(ticket))
-        # if CorrTick == 'yes' and len(ticketNums) > 0 and int(ticketNumber) in (map(int, ticketNums)):
-        #     im.executeModality('TEXT_default', 'Your ticket has been found!')
-        #     say('Your ticket has been found in the database', 'en')
-        #     CorrTick == 'yes'
-        #     # break
-        # elif CorrTick == 'yes' and int(ticketNumber) not in (map(int, ticketNums)):
-        #     im.executeModality('TEXT_default', 'Sorry. Your ticket was not found.')
-        #     say('Your ticket was not found. Let us start again.', 'en')
-        #     im.executeModality('TEXT_default', 'Please enter again the digits of your ticket number one by one.')
-        #     CorrTick = 'no'
+        # First number:
+        im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
+        im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
+        Num1 = im.ask(actionname=None, timeoutvalue=1000)
+        say('number '+Num1)
+        im.display.remove_buttons()
+
+        # Second number:
+        im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
+        im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
+        Num2 = im.ask(actionname=None, timeoutvalue=200)
+        say('number'+Num2)
+        im.display.remove_buttons()
+
+        # Second number:
+        im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
+        im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
+        Num3 = im.ask(actionname=None, timeoutvalue=200)
+        say('number'+Num3)
+        im.display.remove_buttons()
+
+        # Final ticket number
+        ticketNumber = str(Num1 + Num2 + Num3)
+        time.sleep(1)
+        ticketStr = 'Your ticket number is: ' + ticketNumber
+        im.executeModality('TEXT_default', ticketStr)
+        say('Your ticket number is '+ticketNumber)
+
+        # Check if ticket was entered correctly
+        im.executeModality('BUTTONS',[['yes','Yes'],['no','No']])
+        im.executeModality('ASR',['yes','no'])
+        CorrTick = im.ask(actionname=None, timeoutvalue=100)
+        im.display.remove_buttons()
+        if CorrTick == 'yes':
+            say('Great! Let me look at your information')
+        elif CorrTick == 'no':
+            im.executeModality('TEXT_default', 'Please enter again the digits of your ticket number one by one.')
+            say('Sorry about that. Let us try again')
+
+        # Check the tickets in the system
+        directory = "/home/ubuntu/playground/HumanRobotInteraction_ER/patientInfo"
+
+        ticketNums = []
+        with open(os.path.join(directory, "PatientTicketNum.txt"), "r") as patientTicketNums:
+            for ticket in patientTicketNums.readlines():
+                ticketNums.append(str(ticket))
+        if CorrTick == 'yes' and len(ticketNums) > 0 and int(ticketNumber) in (map(int, ticketNums)):
+            im.executeModality('TEXT_default', 'Your ticket has been found!')
+            say('Your ticket has been found in the database', 'en')
+            CorrTick == 'yes'
+            # break
+        elif CorrTick == 'yes' and int(ticketNumber) not in (map(int, ticketNums)):
+            im.executeModality('TEXT_default', 'Sorry. Your ticket was not found.')
+            say('Your ticket was not found. Let us start again.', 'en')
+            im.executeModality('TEXT_default', 'Please enter again the digits of your ticket number one by one.')
+            CorrTick = 'no'
 
     # Retrieve the info of user in database
     RecordDict = dict()
@@ -441,6 +379,53 @@ def i2():
 def i3():
     begin()
     im.display.loadUrl('ERnewpatient.html')
+
+    im.executeModality('TEXT_default', 'Please enter the digits of your ticket number one by one.')
+    say('Let me look for you in the database. Please enter your ticket number', 'en')
+
+    # There are three digits in the ticket number, check one by one with buttons
+    CorrTick = 'no'
+    while CorrTick == 'no':
+        # First number:
+        im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
+        im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
+        Num1 = im.ask(actionname=None, timeoutvalue=1000)
+        say('number '+Num1)
+        im.display.remove_buttons()
+
+        # Second number:
+        im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
+        im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
+        Num2 = im.ask(actionname=None, timeoutvalue=200)
+        say('number'+Num2)
+        im.display.remove_buttons()
+
+        # Second number:
+        im.executeModality('BUTTONS',[['0','0'],['1','1'],['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9']])
+        im.executeModality('ASR',['0','1','2', '3', '4', '5', '6', '7', '8', '9'])
+        Num3 = im.ask(actionname=None, timeoutvalue=200)
+        say('number'+Num3)
+        im.display.remove_buttons()
+
+        # Final ticket number
+        ticketNumber = str(Num1 + Num2 + Num3)
+        time.sleep(1)
+        ticketStr = 'Your ticket number is: ' + ticketNumber
+        im.executeModality('TEXT_default', ticketStr)
+        say('Your ticket number is '+ticketNumber)
+
+        # Check if ticket was entered correctly
+        im.executeModality('BUTTONS',[['yes','Yes'],['no','No']])
+        im.executeModality('ASR',['yes','no'])
+        CorrTick = im.ask(actionname=None, timeoutvalue=100)
+        im.display.remove_buttons()
+        if CorrTick == 'yes':
+            say('Great! Let us add your information to your record')
+        elif CorrTick == 'no':
+            im.executeModality('TEXT_default', 'Please enter again the digits of your ticket number one by one.')
+            say('Sorry about that. Let us try again')
+
+
     im.executeModality('TEXT_title','Add information to your Patient Record')
     im.executeModality('TEXT_default', 'Please answer the following questions')
     say('Please enter the information being asked by pressing the corresponding buttons', 'en')
@@ -754,6 +739,13 @@ def i3():
     recFile.write(stringDic)
     recFile.close()
 
+    # Check the tickets in the system
+    directory = "/home/ubuntu/playground/HumanRobotInteraction_ER/patientInfo"
+    RecordTxt = ticketNumber + ".txt"
+    recFile = open(os.path.join(directory, RecordTxt), "w+")
+    recFile.write(stringDic)
+    recFile.close()
+
 
     end()
 
@@ -761,7 +753,6 @@ def i3():
 
 mc.setDemoPath('/home/ubuntu/playground/HumanRobotInteraction_ER')
 mc.store_interaction(i2)
-mc.store_interaction(getTicket)
 mc.store_interaction(i1)
 mc.store_interaction(i0)
 mc.store_interaction(i3)
